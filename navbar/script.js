ef30d351f942e2216c65b6f6617c23a8341c3ea4
deepak <deepakdpyqaz@gmail.com>
2022-07-05 23:29:55 +0530

    fixing navbar

---
+++ navbar/script.js
@@ -0,0 +1,5 @@
+const menubtn =  document.getElementById("aps_nav_menubtn");
+const sidebar = document.getElementById("aps_sidebar");
+menubtn.addEventListener("click",()=>{
+    sidebar.classList.toggle("active");
+})
