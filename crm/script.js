9e0bb971e87867bb29987807084ae3dd08b907f0
deepak <deepakdpyqaz@gmail.com>
2022-07-06 01:24:54 +0530

    adding update_form

--- crm/script.js
+++ crm/script.js
@@ -8,3 +8,13 @@ window.onload = function(){
         sidebar.classList.toggle("active");
     }
 }
+document.querySelectorAll(".crm_form fieldset").forEach((elem)=>{
+    try{
+        let lbl = elem.querySelector("label");
+        let ip = lbl.nextElementSibling;
+        if(ip.hasAttribute("required") && lbl.innerHTML.indexOf("*")==-1){
+            lbl.innerHTML += " *";
+        }
+    }catch{
+    }
+})
